01a8bfc77d0bf114a50852addf7fd52cc7d50c63
lyndon santos <santos.lyndon@gmail.com>
2020-07-27 07:11:26 -0700

    initial commit

---
+++ PizzaStore/PizzaStore.Client/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using PizzaStore.Domain.Models;
+
+namespace PizzaStore.Client
+{
+  class Program
+  {
+    static void Main()
+    {
+      Welcome();
+    }
+
+    static void Welcome()
+    {
+      System.Console.WriteLine("Welcome to PizzaWorld");
+      System.Console.WriteLine("Best Pizza in the World");
+      System.Console.WriteLine();
+
+      System.Console.WriteLine("Select 1 for Client Orders");
+      System.Console.WriteLine("Select 2 for Store Management");
+      System.Console.WriteLine("Press any other key to exit");
+      System.Console.WriteLine();
+
+      int selection;
+      int.TryParse(Console.ReadLine(), out selection);
+
+      switch (selection)
+      {
+        case 1:
+          var starter = new Starter();
+          var user = new User();
+          var store = new Store();
+
+          try
+          {
+            Client.Menu(starter.CreateOrder(user, store));
+          }
+          catch (Exception ex)
+          {
+            System.Console.WriteLine(ex.Message);
+          }
+
+          break;
+        case 2:
+          try
+          {
+            Stores.Menu();
+          }
+          catch (Exception ex)
+          {
+            System.Console.WriteLine(ex.Message);
+          }
+
+          break;
+        default:
+          break;
+      }
+
+    }
+  }
+}
---
+++ PizzaStore/PizzaStore.Client/Starter.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using PizzaStore.Domain.Models;
+
+namespace PizzaStore.Client
+{
+  public class Starter
+  {
+    public Order CreateOrder(User user, Store store)
+    {
+      try
+      {
+        var order = new Order();
+
+        user.Orders.Add(order);
+        store.Orders.Add(order);
+
+        return order;
+      }
+      catch
+      {
+        //return null;
+        throw new System.Exception("we messed up");
+      }
+      finally
+      {
+        GC.Collect();
+      }
+    }
+
+    public static void PrintMenu()
+    {
+      System.Console.WriteLine("Select 1 for Cheese Pizza");
+      System.Console.WriteLine("Select 2 for Pepperoni Pizza");
+      System.Console.WriteLine("Select 3 for Hawaiian Pizza");
+      System.Console.WriteLine("Select 4 for Custom Pizza");
+      System.Console.WriteLine("Select 5 for Show Cart");
+      System.Console.WriteLine("Select 6 to Save your Order & Exit");
+      System.Console.WriteLine("Select 7 to Show Previously Saved Orders");
+      System.Console.WriteLine("Select 8 to Exit without Saving Your Orders");
+      System.Console.WriteLine();
+    }
+
+    public static void ChooseSize()
+    {
+      System.Console.WriteLine("Select 1 for Family");
+      System.Console.WriteLine("Select 2 for Large");
+      System.Console.WriteLine("Select 3 for Regular");
+      System.Console.WriteLine();
+    }
+
+    public static void ChooseCrust()
+    {
+      System.Console.WriteLine("Select 1 for Thin");
+      System.Console.WriteLine("Select 2 for Thick");
+      System.Console.WriteLine("Select 3 for Stuffed");
+      System.Console.WriteLine();
+    }
+    public static void MgmtMenu()
+    {
+      System.Console.WriteLine("Select 1 for Sales History");
+      System.Console.WriteLine("Select 2 for Order History");
+      System.Console.WriteLine("Press any other key to exit");
+      System.Console.WriteLine();
+    }
+  }
+}
